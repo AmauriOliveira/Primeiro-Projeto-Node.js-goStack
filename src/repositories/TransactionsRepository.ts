9275936c3a79713483627efaa2cb699fe2c0f22d
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-08-27 18:50:30 -0300

    Alls Test  PASS

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const res = this.transactions;
+
+    let income = 0;
+    let outcome = 0;
+
+    res.forEach((item: Transaction) => {
+      item.type === 'income' ? (income += item.value) : (outcome += item.value);
+    });
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: RequestDTO): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (total < value && type === 'outcome') {
+      throw Error(
+        'The requested amount above the available amount in the account',
+      );
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
